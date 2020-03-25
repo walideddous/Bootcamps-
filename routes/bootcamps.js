9aac75d9949e74ec4d134a2230647ae6604ceaf3
walid <walid.eddous@gmail>
2020-03-25 03:25:13 +0100

    adding data to the database and adding Errohandler

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,30 +1,21 @@
 const express = require("express");
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp
+} = require("../controllers/bootcamps");
 const router = express.Router();
 
-router.get("/api/v1/bootcamp", (req, res) => {
-  res.status(200).json({ sucess: true, msg: "Show all bootcamps" });
-});
-
-router.get("/api/v1/bootcamp/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: `Show bootcamp by id :${req.params.id}` });
-});
-
-router.post("/api/v1/bootcamp", (req, res) => {
-  res.status(200).json({ sucess: true, msg: "Create new bootcamp" });
-});
-
-router.put("/api/v1/bootcamp/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: `update bootcamp by id:${req.params.id}` });
-});
-
-router.delete("/api/v1/bootcamp/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: `Delete bootcamp by id:${req.params.id}` });
-});
+router
+  .route("/")
+  .get(getBootcamps)
+  .post(createBootcamp);
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
